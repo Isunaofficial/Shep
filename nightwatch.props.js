15345d28d4d30e671b1333e939e9170641588348
Isunaofficial <isunaofficial@gmail.com>
2019-08-07 11:48:34 -0600

    dfdsf

---
+++ nightwatch.props.js
@@ -0,0 +1,6 @@
+module.exports = {
+    resourcePath: "C:\\Users\\isuna\\Documents\\Devmountain\\testing-resources\\",
+    seleniumServer: "selenium-server-standalone-3.141.59.jar",
+    chromedriver: "chromedriver.exe"
+ }
+ 
